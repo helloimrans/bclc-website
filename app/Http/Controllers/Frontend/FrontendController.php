74fa5a466ad2fc6a7db08021b1f82488b6c6ae36
MD. IMRAN ALI <imranbd.dev@gmail.com>
2023-03-29 22:15:00 +0600

    frontend folder structure changed

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -23,13 +23,13 @@ class FrontendController extends Controller
         $data['newses'] = Abrwn::where('is_abrwn', 5)->where('status', 1)->limit(10)->latest()->get();
         $data['blogs'] = Abrwn::where('is_abrwn', 2)->where('status', 1)->limit(10)->latest()->get();
         $data['insights'] = AssociatedService::where('status', 1)->limit(10)->latest()->get();
-        return view('frontend.pages.index', $data);
+        return view('frontend.home.index', $data);
     }
 
     public function serviceCategory()
     {
         $data['service_category'] = ServiceCategory::where('status', 1)->where('is_service', 1)->orderBy('sort', 'ASC')->get();
-        return view('frontend.pages.service_category', $data);
+        return view('frontend.service.service_category', $data);
     }
 
     public function categoryServiceDetails($slug)
@@ -41,7 +41,7 @@ class FrontendController extends Controller
 
         $data['service_count'] = Service::where('service_category_id', $cat_id)->where('is_service', 1)->count();
 
-        return view('frontend.pages.service_details', $data);
+        return view('frontend.service.service_details', $data);
     }
     public function serviceDetails($slug)
     {
@@ -50,13 +50,13 @@ class FrontendController extends Controller
 
         $data['service_count'] = Service::where('slug', $slug)->count();
 
-        return view('frontend.pages.service_details', $data);
+        return view('frontend.service.service_details', $data);
     }
 
     public function probonoCategory()
     {
         $data['service_category'] = ServiceCategory::where('status', 1)->where('is_pro_bono', 1)->orderBy('sort', 'ASC')->get();
-        return view('frontend.pages.probono_category', $data);
+        return view('frontend.pro_bono.probono_category', $data);
     }
 
     public function categoryProbonoDetails($slug)
@@ -68,7 +68,7 @@ class FrontendController extends Controller
 
         $data['service_count'] = Service::where('service_category_id', $cat_id)->where('is_service', 2)->count();
 
-        return view('frontend.pages.probono_details', $data);
+        return view('frontend.pro_bono.probono_details', $data);
     }
     public function probonoDetails($slug)
     {
@@ -77,140 +77,115 @@ class FrontendController extends Controller
 
         $data['service_count'] = Service::where('slug', $slug)->count();
 
-        return view('frontend.pages.probono_details', $data);
+        return view('frontend.pro_bono.probono_details', $data);
     }
     public function articles()
     {
         $data['articles'] = Abrwn::where('is_abrwn', 1)->where('status', 1)->latest()->paginate(8);
-        return view('frontend.pages.article', $data);
+        return view('frontend.article.article', $data);
     }
     public function writeup()
     {
         $data['writeups'] = Abrwn::where('is_abrwn', 4)->where('status', 1)->latest()->paginate(8);
-        return view('frontend.pages.writeup', $data);
+        return view('frontend.writeup.writeup', $data);
     }
     public function blog()
     {
         $data['blogs'] = Abrwn::where('is_abrwn', 2)->where('status', 1)->latest()->paginate(8);
-        return view('frontend.pages.blog', $data);
+        return view('frontend.blog.blog', $data);
     }
     public function news()
     {
         $data['newses'] = Abrwn::where('is_abrwn', 5)->where('status', 1)->latest()->paginate(8);
-        return view('frontend.pages.news', $data);
+        return view('frontend.news.news', $data);
     }
     public function review()
     {
         $data['reviews'] = Abrwn::where('is_abrwn', 3)->where('status', 1)->latest()->paginate(8);
-        return view('frontend.pages.review', $data);
+        return view('frontend.review.review', $data);
     }
     public function insights()
     {
         $data['insights'] = AssociatedService::where('status', 1)->latest()->paginate(8);
-        return view('frontend.pages.insights', $data);
+        return view('frontend.insights.insights', $data);
     }
     public function articleDetails($slug)
     {
         $data['article'] = Abrwn::where('slug', $slug)->first();
         $data['relatedArticles'] = Abrwn::where('is_abrwn', 1)->where('abrwn_category_id',$data['article']->abrwn_category_id)->where('status', 1)->latest()->limit(8)->get();
         $data['latestArticles'] = Abrwn::where('is_abrwn', 1)->where('status', 1)->latest()->limit(8)->get();
-        return view('frontend.pages.article_details',$data);
+        return view('frontend.article.article_details',$data);
     }
     public function writeupDetails($slug)
     {
         $data['writeup'] = Abrwn::where('slug', $slug)->first();
         $data['relatedWriteups'] = Abrwn::where('is_abrwn', 4)->where('abrwn_category_id',$data['writeup']->abrwn_category_id)->where('status', 1)->latest()->limit(8)->get();
         $data['latestWriteups'] = Abrwn::where('is_abrwn', 4)->where('status', 1)->latest()->limit(8)->get();
-        return view('frontend.pages.writeup_details',$data);
+        return view('frontend.writeup.writeup_details',$data);
     }
     public function newsDetails($slug)
     {
         $data['news'] = Abrwn::where('slug', $slug)->first();
         $data['relatedNewses'] = Abrwn::where('is_abrwn', 5)->where('abrwn_category_id',$data['news']->abrwn_category_id)->where('status', 1)->latest()->limit(8)->get();
         $data['latestNewses'] = Abrwn::where('is_abrwn', 5)->where('status', 1)->latest()->limit(8)->get();
-        return view('frontend.pages.news_details',$data);
+        return view('frontend.news.news_details',$data);
     }
     public function blogDetails($slug)
     {
         $data['blog'] = Abrwn::where('slug', $slug)->first();
         $data['relatedBlogs'] = Abrwn::where('is_abrwn', 2)->where('abrwn_category_id',$data['blog']->abrwn_category_id)->where('status', 1)->latest()->limit(8)->get();
         $data['latestBlogs'] = Abrwn::where('is_abrwn', 2)->where('status', 1)->latest()->limit(8)->get();
-        return view('frontend.pages.blog_details',$data);
+        return view('frontend.blog.blog_details',$data);
     }
     public function insightsDetails($slug)
     {
         $data['insight'] = AssociatedService::where('slug', $slug)->first();
         $data['latestInsights'] = AssociatedService::where('status', 1)->latest()->limit(10)->get();
-        return view('frontend.pages.insights_details',$data);
+        return view('frontend.insights.insights_details',$data);
     }
     public function lawsRules(){
         $data['categories'] = LawCategory::with('laws')->where('status',1)->orderBy('sort','ASC')->get();
-        return view('frontend.pages.laws_rules',$data);
+        return view('frontend.laws_and_rules.laws_rules',$data);
     }
 
     public function lawsRulesDetails($slug){
         $data['law'] = Law::with('actChapter','rulesChapter')->where('slug',$slug)->first();
         // $data['law']->increment('total_views');
         // $data['categories'] = LawCategory::where('status', 1)->orderBy('sort', 'ASC')->get();
-        return view('frontend.pages.laws_rules_details',$data);
+        return view('frontend.laws_and_rules.laws_rules_details',$data);
     }
     public function lawsRulesView($slug){
         $data['law'] = Law::with('actChapter','rulesChapter')->where('slug',$slug)->first();
         $data['law']->increment('total_views');
         $data['categories'] = LawCategory::where('status', 1)->orderBy('sort', 'ASC')->get();
-        return view('frontend.pages.laws_rules_view',$data);
+        return view('frontend.laws_and_rules.laws_rules_view',$data);
     }
 
     public function lawsRulesChapter($slug){
         $data['chapter'] = LawChapter::with('law')->where('slug',$slug)->first();
-        return view('frontend.pages.laws_rules_chapter',$data);
+        return view('frontend.laws_and_rules.laws_rules_chapter',$data);
     }
 
 
     public function officeFunction(){
         $data['officeCategories'] = OfficeCategory::where('status',1)->get();
-        return view('frontend.pages.office_function',$data);
+        return view('frontend.office_function.office_function',$data);
     }
 
     public function serviceFacility(){
         $data['serviceCategories'] = ServiceFacilityCategory::where('status',1)->get();
-        return view('frontend.pages.service_facility',$data);
+        return view('frontend.service_facility.service_facility',$data);
     }
 
     public function courses()
     {
-        return view('frontend.pages.training.courses');
+        return view('frontend.training.courses');
     }
 
 
     public function courseDetails($slug)
     {
-        return view('frontend.pages.training.course_details');
+        return view('frontend.training.course_details');
     }
-
-    
-    public function Registerlearner(){
-    
-        return view('frontend.pages.register');
-     }
-    
-
-     
-    public function ExpertRegistration(){
-    
-        return view('frontend.pages.expart_registration');
-     }
-
-      
-    public function SubscriberRegistration(){
-    
-        return view('frontend.pages.subscrib_registration');
-     }
-
-    // public function Registerlearner($slug)
-    // {
-    //     return view('');
-    // }
-
 
 }
