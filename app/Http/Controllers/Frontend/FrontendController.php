4a0bd3d13eb15e6ecca9a5e139aaed414d7a9c72
mrsharifull <116838065+mrsharifull@users.noreply.github.com>
2023-04-01 02:09:57 +0600

    Course & details show on front --updated

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -11,6 +11,7 @@ use App\Models\LawChapter;
 use App\Models\Service;
 use App\Models\ServiceCategory;
 use App\Models\OfficeCategory;
+use App\Models\Course;
 use App\Models\PrivacyPolicy;
 use App\Models\ServiceFacilityCategory;
 use App\Models\TermsCondition;
@@ -181,12 +182,14 @@ class FrontendController extends Controller
 
     public function courses()
     {
-        return view('frontend.training.courses');
+        $data['courses'] = Course::all();
+        return view('frontend.training.courses', $data);
     }
 
     public function courseDetails($slug)
     {
-        return view('frontend.training.course_details');
+        $course = Course::where('slug', $slug)->first();
+        return view('frontend.training.course_details', compact('course'));
     }
 
     public function termsCondition()
@@ -204,5 +207,4 @@ class FrontendController extends Controller
     {
         return view('frontend.contact_us.contact_us');
     }
-
 }
