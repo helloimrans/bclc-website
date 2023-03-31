a9f787e3a48b123b37feaca034ef463fe82a7f0b
majharul <majharul.islam1657@gmail.com>
2023-03-31 23:58:23 +0600

    nothing

--- resources/views/admin/layouts/inc/sidebar.blade.php
+++ resources/views/admin/layouts/inc/sidebar.blade.php
@@ -258,6 +258,20 @@
                 </ul>
             </li>
 
+ 
+              <!-- contact message -->
+            
+            <li class="nav-item"><a class="d-flex align-items-center" href="#"><i class="fa fa-cog"></i><span
+                        class="menu-title text-truncate" data-i18n="Roles &amp; Permission">contact message</span></a>
+                <ul class="menu-content">
+                    <li class=" "><a class="d-flex align-items-center"
+                            href="{{ route('terms.condition.index') }}"><i data-feather="circle"></i><span
+                                class="menu-item text-truncate" data-i18n="Roles">contact message</span></a>
+                    </li>
+                    
+                </ul>
+            </li>
+
         </ul>
     </div>
 </div>
