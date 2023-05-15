1f1603cbb076a3e3960b737c778e69607fdb27cb
MD. IMRAN ALI <imranbd.dev@gmail.com>
2023-05-16 03:37:46 +0600

    create orders and course order details migration

---
+++ app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Order extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'user_id',
+        'invoice_no',
+        'purpose_of_payment',
+        'payment_method',
+        'account_number',
+        'user_account_number',
+        'amount',
+        'transaction_id',
+        'currency',
+        'status',
+    ];
+}
