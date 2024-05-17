f2967d71c91a1272334d6040b9f0e4f06f341584
MD. IMRAN ALI <imranbd.dev@gmail.com>
2024-05-17 11:32:46 +0600

    switch bangla to first laws both lang view

--- resources/views/frontend/laws_and_rules/part_details/laws_rules_part_both.blade.php
+++ resources/views/frontend/laws_and_rules/part_details/laws_rules_part_both.blade.php
@@ -2,63 +2,6 @@
     <div class="col-md-6">
         <div class="laws-box">
             <ul class="law-chapter-summery pt-3">
-                <h5 class="mb-3">Sections (English)</h5>
-                @foreach ($part->section->where('status', 1)->where('parent_id', 0) as $section)
-                    <li>
-                        <a href="#goSection_en_{{ $section->slug }}">
-                            {{ session()->get('lawLocale') == 'bn' ? $section->title_bn : $section->title }}
-                        </a>
-                    </li>
-                    @if (count($section->childs->where('status', 1)))
-                        @include('frontend.laws_and_rules.child_section_intro_bn', [
-                            'childs' => $section->childs,
-                        ])
-                    @endif
-                @endforeach
-            </ul>
-
-            <div class="laws-chapters-one">
-                <h5>
-                    {{ $part->part_no }}
-                    :
-                    {{ $part->title }}
-
-                    <span class="text-uppercase">(
-
-                        @if ($part->is_act == 1)
-                            {{ $part->law->short_form }}
-                        @endif
-
-                        @if ($part->is_rules == 1)
-                            {{ $part->law->rules_short_form }}
-                        @endif
-
-                        )
-                    </span>
-                </h5>
-            </div>
-            <div class="laws-chapters mt-4">
-                @if ($part->section->where('status', 1)->where('parent_id', 0)->count() == 0)
-                    <p class="text-danger text-14 text-center">Not found section!</p>
-                @endif
-            </div>
-            @foreach ($part->section->where('status', 1)->where('parent_id', 0) as $section)
-                <div class="laws-chapters-section" id="goSection_en_{{ $section->slug }}">
-                    <h5>{{ $section->title }}</h5>
-                    <p>{!! $section->description !!}</p>
-                </div>
-                @if (count($section->childs->where('status', 1)))
-                    @include('frontend.laws_and_rules.child_section_en', [
-                        'childs' => $section->childs,
-                    ])
-                @endif
-            @endforeach
-        </div>
-
-    </div>
-    <div class="col-md-6">
-        <div class="laws-box">
-            <ul class="law-chapter-summery pt-3">
                 <h5 class="mb-3">Sections (Bangla)</h5>
                 @foreach ($part->section->where('status', 1)->where('parent_id', 0) as $section)
                     <li>
@@ -106,6 +49,63 @@
                 </div>
                 @if (count($section->childs->where('status', 1)))
                     @include('frontend.laws_and_rules.child_section_bn', [
+                        'childs' => $section->childs,
+                    ])
+                @endif
+            @endforeach
+        </div>
+
+    </div>
+    <div class="col-md-6">
+        <div class="laws-box">
+            <ul class="law-chapter-summery pt-3">
+                <h5 class="mb-3">Sections (English)</h5>
+                @foreach ($part->section->where('status', 1)->where('parent_id', 0) as $section)
+                    <li>
+                        <a href="#goSection_en_{{ $section->slug }}">
+                            {{ session()->get('lawLocale') == 'bn' ? $section->title_bn : $section->title }}
+                        </a>
+                    </li>
+                    @if (count($section->childs->where('status', 1)))
+                        @include('frontend.laws_and_rules.child_section_intro_bn', [
+                            'childs' => $section->childs,
+                        ])
+                    @endif
+                @endforeach
+            </ul>
+
+            <div class="laws-chapters-one">
+                <h5>
+                    {{ $part->part_no }}
+                    :
+                    {{ $part->title }}
+
+                    <span class="text-uppercase">(
+
+                        @if ($part->is_act == 1)
+                            {{ $part->law->short_form }}
+                        @endif
+
+                        @if ($part->is_rules == 1)
+                            {{ $part->law->rules_short_form }}
+                        @endif
+
+                        )
+                    </span>
+                </h5>
+            </div>
+            <div class="laws-chapters mt-4">
+                @if ($part->section->where('status', 1)->where('parent_id', 0)->count() == 0)
+                    <p class="text-danger text-14 text-center">Not found section!</p>
+                @endif
+            </div>
+            @foreach ($part->section->where('status', 1)->where('parent_id', 0) as $section)
+                <div class="laws-chapters-section" id="goSection_en_{{ $section->slug }}">
+                    <h5>{{ $section->title }}</h5>
+                    <p>{!! $section->description !!}</p>
+                </div>
+                @if (count($section->childs->where('status', 1)))
+                    @include('frontend.laws_and_rules.child_section_en', [
                         'childs' => $section->childs,
                     ])
                 @endif
--- resources/views/frontend/laws_and_rules/search_result_details/search_result_details_both.blade.php
+++ resources/views/frontend/laws_and_rules/search_result_details/search_result_details_both.blade.php
@@ -1,27 +1,5 @@
 <div class="row">
-    <div class="col-md-6">
-        <div class="laws-chapters-section laws-box">
-            <div class="row mb-2">
-                <div class="col-8">
-                    <h5 class="mb-2 text-18">{{ $section->title }}</h5>
-                    <h5 class="mb-3 text-15">{{ $section->chapter->chapter_no }} :
-                        {{ $section->chapter->title }}</h5>
-                </div>
-                <div class="col-4 align-self-center text-right">
-                    <p class="mb-0 font-weight-bold text-17 td">
-                        @if ($section->is_act == 1)
-                            {{ $law->short_form }}
-                        @endif
-
-                        @if ($section->is_rules == 1)
-                            {{ $law->rules_short_form }}
-                        @endif
-                    </p>
-                </div>
-            </div>
-            <p class="mb-0">{!! $section->description !!}</p>
-        </div>
-    </div>
+    {{-- bangla --}}
     <div class="col-md-6">
         <div class="laws-chapters-section laws-box">
             <div class="row mb-2">
@@ -43,6 +21,31 @@
                 </div>
             </div>
             <p class="mb-0">{!! $section->description_bn !!}</p>
+        </div>
+    </div>
+
+    {{-- english --}}
+    <div class="col-md-6">
+        <div class="laws-chapters-section laws-box">
+            <div class="row mb-2">
+                <div class="col-8">
+                    <h5 class="mb-2 text-18">{{ $section->title }}</h5>
+                    <h5 class="mb-3 text-15">{{ $section->chapter->chapter_no }} :
+                        {{ $section->chapter->title }}</h5>
+                </div>
+                <div class="col-4 align-self-center text-right">
+                    <p class="mb-0 font-weight-bold text-17 td">
+                        @if ($section->is_act == 1)
+                            {{ $law->short_form }}
+                        @endif
+
+                        @if ($section->is_rules == 1)
+                            {{ $law->rules_short_form }}
+                        @endif
+                    </p>
+                </div>
+            </div>
+            <p class="mb-0">{!! $section->description !!}</p>
         </div>
     </div>
 </div>
